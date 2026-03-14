11344ff9c2c5a66a5e5f93acb653c6bb49e9c8ce
Anish <anish.madhyastha@gmail.com>
2026-03-14 07:52:10 +0530

    translation feature

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 export default function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router} />
+      <LanguageProvider>
+        <RouterProvider router={router} />
+      </LanguageProvider>
     </AuthProvider>
   );
 }
